2793e5aab94fa675736040f05c99b66002db9db5
Sonu <chaudharysonu2614@gmail.com>
2023-08-14 10:11:21 +0530

    button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// import index from "./index";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const Buttonclick =() =>{
+    let [count,setCount]=useState(0)
+
+
+    return(
+        <div>
+            <p>Button clicked {count} times</p>
+            <button onClick={()=>setCount(count+1)}>Click Me</button>
+        </div>
+    )
 }
-
-export default App;
+export default Buttonclick;
